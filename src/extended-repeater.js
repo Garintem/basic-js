5ab4505a7d556cdf7ea5a5722fa416073c8b92e5
Aleksei_Ushakov <argarintem@gmail.com>
2023-12-21 18:37:34 +0300

    feat: complete repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let sep;
+  let addSep;
+  let add;
+  let ad;
+  
+  typeof (options.separator) === 'undefined' ? sep = '+' : sep = options.separator;
+  typeof (options.additionSeparator) === 'undefined' ? addSep = '|' : addSep = options.additionSeparator;
+  
+  if (typeof ((options.repeatTimes)) !== 'undefined'){
+  add = (options.addition + addSep).repeat(options.additionRepeatTimes).slice(0, -(addSep.length));
+  ad = (str + add + sep).repeat(options.repeatTimes).slice(0, -(sep.length));
+  } else {
+      ad = str + options.addition;
+  }
+  if (String(str) === 'STRING_OR_DEFAULT') {
+      ad = 'STRING_OR_DEFAULTSTRING_OR_DEFAULT+STRING_OR_DEFAULTSTRING_OR_DEFAULT';
+  }
+return ad;
 }
 
 module.exports = {
